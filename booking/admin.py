839a6f216d9ba7fa97be5030cc76e4df70c1d06a
mbonabucya <mbonajames12@gmail.com>
2023-03-04 13:21:23 -0500

    add the browsable end points using restframework

--- booking/admin.py
+++ booking/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-#admin.site.register(Salon)
-admin.site.register(Stylist)
-admin.site.register(Booking)
 
 
 class SalonAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'address')
     search_fields = ('name',)
-
 admin.site.register(Salon, SalonAdmin)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display =('id','stylist','customer_name','appointment_time')
+    search_fields = ('stylist',)
+admin.site.register(Booking, BookingAdmin)
+
+class StylistAdmin(admin.ModelAdmin):
+    list_display =('id','name','salon','email','phone_number')
+    search_fields = ('salon',)
+admin.site.register(Stylist,StylistAdmin)
--- salon_booking/urls.py
+++ salon_booking/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('booking.url')),
+    path('', include('booking.url')),
 ]
